74ed5c4242809b3cae8c6d6d6b4ae549b630b764
Elias Souza <97631394+eliasnsz@users.noreply.github.com>
2022-09-26 18:33:36 -0300

    .

--- src/components/MainPageComponents/FeatureCard/FeatureCard.jsx
+++ src/components/MainPageComponents/FeatureCard/FeatureCard.jsx
@@ -1,13 +1,12 @@
 import './FeatureCard.scss'
 
-export default function FeatureCard({ href, img, title, desc, delay }) {
+export default function FeatureCard({  img, title, desc, delay }) {
   return (
     <div  
       data-aos="zoom-in" 
       data-aos-duration="500" 
       data-aos-delay={delay} d
       ata-aos-easing="ease-out"
-      onClick={() => window.location = `${href}`}
     >
       <div id="feature-card">
         <div className="img-container">
--- src/components/MainPageComponents/Features/Features.jsx
+++ src/components/MainPageComponents/Features/Features.jsx
@@ -1,6 +1,8 @@
 import './Features.scss'
 import FeatureCard from '../FeatureCard/FeatureCard'
 
+import { Link } from 'react-router-dom'
+
 export default function Features() {
   return (
     <>
@@ -13,34 +15,42 @@ export default function Features() {
             <hr />
           </div>
           <div className="grid-container">
-            <FeatureCard
-              href={"/agentes"}
-              img={"https://img.icons8.com/material-sharp/344/conference-call.png"}
-              title={"Agentes"}
-              desc={"Conheça os agentes e suas habilidades."}
-              delay={"100"}
-            />
-            <FeatureCard
-              href={"/mapas"}
-              img={"https://cdn-icons-png.flaticon.com/512/535/535263.png"}
-              title={"Mapas"}
-              desc={"Conheça os mapas de Valorant."}
-              delay={"150"}
-            />
-            <FeatureCard
-              href={"/armas"}
-              img={"https://cdn-icons-png.flaticon.com/512/486/486256.png"}
-              title={"Armas"}
-              desc={"Conheça todos os armamentos de Valorant."}
-              delay={"200"}
-            />
-            <FeatureCard
-              href={"/skins"}
-              img={"https://cdn-icons-png.flaticon.com/512/5357/5357746.png"}
-              title={"Skins"}
-              desc={"Conheça as diferentes aparências das armas."}
-              delay={"250"}
-            />
+            <Link to="/agentes">
+              <FeatureCard
+                img={"https://img.icons8.com/material-sharp/344/conference-call.png"}
+                title={"Agentes"}
+                desc={"Conheça os agentes e suas habilidades."}
+                delay={"100"}
+              />
+            </Link>
+
+            <Link to="/mapas">
+              <FeatureCard
+                img={"https://cdn-icons-png.flaticon.com/512/535/535263.png"}
+                title={"Mapas"}
+                desc={"Conheça os mapas de Valorant."}
+                delay={"150"}
+              />
+            </Link>
+
+            <Link to="/armas">
+              <FeatureCard
+                img={"https://cdn-icons-png.flaticon.com/512/486/486256.png"}
+                title={"Armas"}
+                desc={"Conheça todos os armamentos de Valorant."}
+                delay={"200"}
+              />
+            </Link>
+
+            <Link to="/skins">
+              <FeatureCard
+                img={"https://cdn-icons-png.flaticon.com/512/5357/5357746.png"}
+                title={"Skins"}
+                desc={"Conheça as diferentes aparências das armas."}
+                delay={"250"}
+              />
+            </Link>
+
           </div>
         </div>
       </section>
