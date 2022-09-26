116e2a53f605fd889b40b1d1d5872c46f24619ea
Elias Souza <97631394+eliasnsz@users.noreply.github.com>
2022-09-26 16:59:15 -0300

    Ajustes de Responsividade

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,6 @@ export default function Footer() {
   return (
     <>
       <footer id='footer'>
-          <p>Riot Games, Valorant e todas as propriedades associadas são marcas comerciais ou marcas registradas da Riot Games, Inc.</p>
           <p>Site criado por <a href="https://eliasnsz.com.br" target="_blank" >Elias Souza</a></p>
       </footer>
     </>
