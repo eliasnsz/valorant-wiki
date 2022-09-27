ef2e434a120b6830340e8053d6c648ef29afb53d
Elias Souza <97631394+eliasnsz@users.noreply.github.com>
2022-09-27 15:52:54 -0300

    .

--- src/components/MapaDisplayComponents/MapDisplay.jsx
+++ src/components/MapaDisplayComponents/MapDisplay.jsx
@@ -4,10 +4,8 @@ export default function MapDisplay({ bgImage, mapImage }) {
   return (
     <>
       <div id="map-display" style={{ backgroundImage: `url(${bgImage})` }}>
-        <div className="limiter">
-          <div className="image-map">
-            {mapImage && <img src={mapImage} alt="" />}
-          </div>
+        <div className="image-map">
+          {mapImage && <img src={mapImage} alt="" />}
         </div>
       </div>
     </>
